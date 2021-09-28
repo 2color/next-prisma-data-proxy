4286824108f170f2c2b6e20e52a7e497b74ccae3
Daniel Norman <daniel@2color.me>
2021-09-28 15:09:44 +0200

    Add error logging and try catch

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -10,7 +10,9 @@ declare global {
   var prisma: PrismaClient | undefined
 }
 
-const prisma = global.prisma || new PrismaClient({})
+const prisma = global.prisma || new PrismaClient({
+  log: ['error']
+})
 
 if (process.env.NODE_ENV !== 'production') global.prisma = prisma
 
--- pages/api/posts/[id]/likes.ts
+++ pages/api/posts/[id]/likes.ts
@@ -7,18 +7,22 @@ export default async function handle(
 ) {
   const postId = req.query.id
   if (req.method === 'PUT') {
-    const post = await prisma.post.update({
-      where: {
-        id: Number(postId),
-      },
-      data: {
-        likes: {
-          increment: 1,
+    try {
+      const post = await prisma.post.update({
+        where: {
+          id: Number(postId),
         },
-      },
-    })
+        data: {
+          likes: {
+            increment: 1,
+          },
+        },
+      })
 
-    res.json({ id: post.id, likes: post.likes })
+      res.json({ id: post.id, likes: post.likes })
+    } catch (e) {
+      console.log(e)
+    }
   } else {
     throw new Error(
       `The HTTP ${req.method} method is not supported at this route.`,
